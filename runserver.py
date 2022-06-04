3f414a9e9460251155832bbd5c8d69fc967697d4
Abdullah Adeel <64294045+mabdullahadeel@users.noreply.github.com>
2022-06-04 22:44:59 +0000

    ✅ setup api server

---
+++ runserver.py
@@ -0,0 +1,11 @@
+import uvicorn
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "app.app:app",
+        host="0.0.0.0",
+        port=8080,
+        log_level="info",
+        reloadT=True
+    )
